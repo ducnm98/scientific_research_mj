21c2932e9719e9f9db8e3e0b88146aba3ddef8f4
John Michael <ducnm.john98@gmail.com>
2018-01-20 16:51:07 +0700

    Update One hidden Player

--- [SR2] Mutilayer_Neural_Network/main.py
+++ [SR2] Mutilayer_Neural_Network/main.py
@@ -12,9 +12,9 @@ class mutiplayer_neural_network:
         self.class_two = 4
         self.class_three = 3
 
-        self.weigth_1 = np.random.random((number_input, self.class_two))
-        self.weigth_2 = np.random.random((self.class_two, self.class_three))
-        self.weigth_3 = np.random.random((self.class_three, 1))
+        self.weigth_1 = 2 * np.random.random((number_input, self.class_two)) - 1
+        self.weigth_2 = 2 * np.random.random((self.class_two, self.class_three)) - 1
+        self.weigth_3 = 2 * np.random.random((self.class_three, 1)) - 1
     def __sigmoid(self, Y):
         #Y là hàm tổng
         return 1 / (1 + np.exp(-Y))
@@ -22,7 +22,7 @@ class mutiplayer_neural_network:
         return Y * (1 - Y)
 
     def train(self, train_input, train_output, interaction):
-        for i in interaction:
+        for i in range(interaction):
             #Bước 1: Forward
             self.One = np.dot(train_input, self.weigth_1)
             self.One = self.__sigmoid(self.One)
@@ -34,13 +34,32 @@ class mutiplayer_neural_network:
             self.Out = self.__sigmoid(self.Out)
 
             #Bước 2: Tính Lỗi
+            Err_Out = (train_output - self.Out) * self.__sigmoid_derivative(self.Out)
 
-            #Tính lỗi tại Output
-            Err_Out = train_output - self.Out
+            #Tính Error
+            Err_Two = np.dot(self.__sigmoid_derivative(self.Two), self.weigth_3)
+            Err_Two = np.dot(Err_Two, Err_Out.T)
+            Err_One = np.dot(self.__sigmoid_derivative(self.One).T, self.weigth_2)
+            Err_One = np.dot(Err_One.T, Err_Two)
 
-            #Tính Denta
+            #Bước 3: Cập nhật trọng số
+            self.weigth_1 += np.dot(train_input, Err_One.T)
+            self.weigth_2 += np.dot(self.Two.T, Err_Two)
+            self.weigth_3 += np.dot(self.Out.T, Err_Out)
 
-            #Tính lỗi tại One và Two
-            Err
+    def think(self, input):
+        print(self.weigth_1)
+        One = self.__sigmoid(np.dot(input, self.weigth_1))
+        Two = self.__sigmoid(np.dot(One, self.weigth_2))
+        return self.__sigmoid(np.dot(Two, self.weigth_3))
 
+
+
+NN = mutiplayer_neural_network(4)
+
+training_set_inputs = np.array([[4.7, 3.2, 1.3, 0.2], [6.1, 2.8, 4.7, 1.2], [5.6, 3.0, 4.1, 1.3], [5.8, 2.7, 5.1, 1.9]])
+training_set_outputs = np.array([[0, 1, 1, 0]]).T
+
+NN.train(training_set_inputs, training_set_outputs, 10000)
+NN.think(NN.think(np.array([6.5, 3.2, 5.1, 2.0])))
 
---
+++ [SR2] Mutilayer_Neural_Network/one_hidden_player.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+class Neural_Network():
+  def __init__(self, inputSize):
+    #parameters
+    np.random.seed(1)
+
+    self.inputSize = inputSize
+    self.hiddenSize = 3
+    #Create random Weights
+    self.weigth_One = np.random.random((self.inputSize, self.hiddenSize))
+    self.weigth_Out = np.random.random((self.hiddenSize, 1))
+
+
+  def __sigmoid(self, s):
+    # activation function
+    return 1/(1+np.exp(-s))
+
+  def __sigmoid_derivative(self, s):
+    #derivative of sigmoid
+    return s * (1 - s)
+
+  def think(self, input_data):
+    #This function also means Forward Step
+    self.One = self.__sigmoid(np.dot(input_data, self.weigth_One))
+    self.Out = self.__sigmoid(np.dot(self.One, self.weigth_Out))
+    return self.Out
+
+  def train (self, train_input, train_output, time):
+    for i in range(time):
+      self.think(train_input)
+      Err_Out = train_output - self.Out
+      Delta_Out = Err_Out * self.__sigmoid_derivative(self.Out)
+
+      Err_One = np.dot(Delta_Out, self.weigth_Out.T)
+      Delta_One = Err_One* self.__sigmoid_derivative(self.One)
+
+      #update Weight
+      self.weigth_One += np.dot(train_input.T, Delta_One )
+      self.weigth_Out += np.dot(self.One.T, Delta_Out)
+
+
+NN = Neural_Network(5)
+
+training_set_inputs = np.array([[1, 4.7, 3.2, 1.3, 0.2], [1, 6.1, 2.8, 4.7, 1.2], [1, 5.6, 3.0, 4.1, 1.3], [1, 5.8, 2.7, 5.1, 1.9], [1, 6.5, 3.2, 5.1, 2.0]])
+training_set_outputs = np.array([[0, 1, 1, 0, 0]]).T
+
+NN.train(training_set_inputs, training_set_outputs, 10000)
+
+print(NN.think(np.array([1, 5.8, 2.7, 3.9, 1.2])))
+
+
+
